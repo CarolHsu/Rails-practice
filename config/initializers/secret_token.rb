b71daaca2dabc2045f7c1fde2350bb9f82b21b16
CarolHsu <sheseee@gmail.com>
2012-05-17 20:25:21 +0800

    This is initial commit for Rails demo.

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Demo::Application.config.secret_token = '608dfbd0a43b0e556b0773a0d28526597eed397c081eb9be87a432d16e389989a934a8cee3729f88608362f9fe49d941f5d78c8423574963dc041eaa4781b744'
