189711816d3a51ebaaa0a140c2543afb8b475bcc
CarolHsu <sheseee@gmail.com>
2012-06-01 20:56:25 +0800

    finished custom order of post

--- app/models/post.rb
+++ app/models/post.rb
@@ -1,5 +1,7 @@
 class Post < ActiveRecord::Base
    belongs_to :user
    belongs_to :board, :counter_cache => true
+   #default_scope :order => 'created_at DESC'
+   scope :recent, :order => 'updated_at DESC'
   attr_accessible :content, :title
 end
